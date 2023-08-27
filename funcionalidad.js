656f78463574c1864a6ebd64fdc341cf42aaf12a
irving-jimenez <142933589+irving-jimenez@users.noreply.github.com>
2023-08-27 13:38:24 -0600

    Update funcionalidad.js

--- funcionalidad.js
+++ funcionalidad.js
@@ -7,17 +7,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     // Validate that all variables exist
     if(toggle && nav && bodypd && headerpd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    nav.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
-    }
+            toggle.addEventListener('click', ()=>{
+            // show navbar
+            nav.classList.toggle('show')
+            // change icon
+            toggle.classList.toggle('bx-x')
+            // add padding to body
+            bodypd.classList.toggle('body-pd')
+            // add padding to header
+            headerpd.classList.toggle('body-pd')
+            })
+        }
     }
     
     showNavbar('header-toggle','nav-bar','body-pd','header')
